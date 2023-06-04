a3007595567afc9ec5274647065b6e8cfab4e7e0
Mateusz Domagalski <mdomagalski@edu.cdv.pl>
2023-06-04 20:00:20 +0200

    AlmostFinishedPapierzPong

---
+++ Assets/Git_Testing/Pong/BallController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Rigidbody2D rb2D;
+    public float speed = 5f;
+    public Vector3 vel;
+    public bool isPlaying;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) && isPlaying == false)
+        {
+            ResetAndSendBallInRandomDirection();
+        }
+        if (rb2D.velocity.magnitude < speed * .5f)
+        {
+            ResetBall();
+        }
+       
+    }
+
+    private void ResetBall()
+    {
+        rb2D.velocity = Vector3.zero;
+        transform.position = Vector3.zero;
+        isPlaying = false;
+    }
+
+    private void ResetAndSendBallInRandomDirection()
+    {
+        ResetBall();
+        rb2D.velocity = GenerateRandomVelocity(true) * speed;
+        vel = rb2D.velocity;
+        isPlaying = true;
+    }
+
+    private Vector3 GenerateRandomVelocity(bool sholudReturnNormalized)
+    {
+        Vector3 velocity = new Vector3();
+        bool shouldGoRight = Random.Range(1, 100) > 50;
+        velocity.x = shouldGoRight ? Random.Range(.8f, .3f) : Random.Range(-.8f, -.3f);
+        velocity.y = shouldGoRight ? Random.Range(.8f, .3f) : Random.Range(-.8f, -.3f);
+        return (sholudReturnNormalized) ? velocity.normalized : velocity;
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rb2D.velocity = Vector3.Reflect(vel, collision.contacts[0].normal);
+        Vector3 newVelocityWithOffset = rb2D.velocity;
+        newVelocityWithOffset += new Vector3(Random.Range(-.5f, 5f), Random.Range(-.5f, 5f));
+        rb2D.velocity = newVelocityWithOffset.normalized * speed;
+        vel = rb2D.velocity;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (transform.position.x > 0)
+            print("Left Player +1");
+
+        if (transform.position.x < 0)
+            print("Right Player +1");
+
+        ResetBall();
+    }
+}
---
+++ Assets/Git_Testing/Pong/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public KeyCode upKey = KeyCode.W;
+    public KeyCode downKey = KeyCode.S;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        if(Input.GetKey(upKey) && transform.position.y <5)
+        {
+            transform.position += Vector3.up * Time.deltaTime * speed;
+        }
+
+        if(Input.GetKey(downKey) && transform.position.y > -5)
+        {
+            transform.position += Vector3.down * Time.deltaTime * speed;
+        }
+
+       
+    }
+}
